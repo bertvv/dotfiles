823e5606b20e76b6b345c22386025d7d3004be2a
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2013-04-09 02:03:19 +0200

    Fixed Vim templates, added Bash functions

---
+++ .bash.d/functions.sh
@@ -0,0 +1,45 @@
+# Bash functions
+#
+
+# Show all the names (CNs and SANs) listed in the SSL certificate
+# for a given domain
+# Credits: https://github.com/mathiasbynens/dotfiles/
+
+function getcertnames() {
+if [ -z "${1}" ]; then
+    echo "ERROR: No domain specified."
+    return 1
+fi
+
+local domain="${1}"
+echo "Testing ${domain}…"
+echo # newline
+
+local tmp=$(echo -e "GET / HTTP/1.0\nEOT" \
+    | openssl s_client -connect "${domain}:443" 2>&1);
+
+if [[ "${tmp}" = *"-----BEGIN CERTIFICATE-----"* ]]; then
+    local certText=$(echo "${tmp}" \
+        | openssl x509 -text -certopt "no_header, no_serial, no_version, \
+        no_signame, no_validity, no_issuer, no_pubkey, no_sigdump, no_aux");
+    echo "Common Name:"
+    echo # newline
+    echo "${certText}" | grep "Subject:" | sed -e "s/^.*CN=//";
+    echo # newline
+    echo "Subject Alternative Name(s):"
+    echo # newline
+    echo "${certText}" | grep -A 1 "Subject Alternative Name:" \
+        | sed -e "2s/DNS://g" -e "s/ //g" | tr "," "\n" | tail -n +2
+    return 0
+else
+    echo "ERROR: Certificate not found.";
+    return 1
+fi
+
+}
+
+# Create a new directory and enter it
+# Credits: https://github.com/mathiasbynens/dotfiles/
+function mkd() {
+mkdir -p "$@" && cd "$@"
+}
