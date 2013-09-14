7b5ba90303c2601557d472c4706bf4fae4a59d35
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2013-09-14 14:29:03 +0200

    Added .bash.d/environment.sh for setting env variables

---
+++ .bash.d/environment.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Created:  za 14 sep 2013 02:28:09  CEST
+# Modified: za 14 sep 2013 02:28:09  CEST
+# Author:   Bert Van Vreckem <bert.vanvreckem@gmail.com>
+#
+# 
+
+export PATH=${HOME}/opt/packer:${PATH}
+
