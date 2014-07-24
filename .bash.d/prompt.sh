5e063087e0e7346bddf5d7ef091ecd51e2c8197a
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2014-07-24 21:36:09 +0200

    Document Powerline-shell install

--- .bash.d/prompt.sh
+++ .bash.d/prompt.sh
@@ -1,5 +1,22 @@
 # Bash prompt with Powerline-shell
 
+# Installation (YMMV)
+# 1/ Install Powerline-shell [1]
+#    $ cd ~/opt/
+#    $ git clone https://github.com/milkbikis/powerline-shell
+#    $ cd powerline-shell
+#    $ ./install.py
+# 2/ Install the font (follow "fontconfig" instructions at [2])
+#    $ cd ~/.fonts/
+#    $ wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+#    $ fc-cache -vf ~/.fonts
+#    $ mkdir ~/.fonts.conf.d/ && cd ~/.fonts.conf.d/
+#    $ wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+# 3/ Restart shell
+#
+# [1] https://github.com/milkbikis/powerline-shell
+# [2] https://powerline.readthedocs.org/en/latest/installation/linux.html#font-installation
+
 #  Powerline-shell is assumed to be installed in $powerline_shell_home
 powerline_shell_home=~/opt/powerline-shell
 
