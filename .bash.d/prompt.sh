ee16fd0e55ec50b878f1d2b6d1bfd865b92814dc
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2014-07-24 21:28:52 +0200

    Use Powerline-shell for the Bash prompt

--- .bash.d/prompt.sh
+++ .bash.d/prompt.sh
@@ -1,28 +1,11 @@
-# Bash prompt
-# Credits:
-#  Colors: https://wiki.archlinux.org/index.php/Color_Bash_Prompt
-#  Prompt with Git status: https://github.com/gf3/dotfiles/
+# Bash prompt with Powerline-shell
 
-# Load color definitions from this directory
-. ${HOME}/.bash.d/colors.sh
+#  Powerline-shell is assumed to be installed in $powerline_shell_home
+powerline_shell_home=~/opt/powerline-shell
 
-# Git status
-function parse_git_dirty() {
-    [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]] && echo "*"
+
+function _update_ps1() {
+  export PS1="$(${powerline_shell_home}/powerline-shell.py $? 2> /dev/null)"
 }
 
-function parse_git_branch() {
-    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
-}
-
-# Root?
-if [[ ${UID} -eq 0 ]]; then
-    UserColor=${BIRed}
-else
-    UserColor=${BIGreen}
-fi
-
-
-# Set the prompt:
-export PS1="\[${UserColor}\]\u\[${White}\]@\[${Yellow}\]\h\[${White}\]:\[${BIBlue}\]\w\[${White}\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" \")[\[${BIPurple}\]\$(parse_git_branch)\[${White}\]]\n\$ \[${Reset}\]"
-export PS2="\[${Yellow}\]→ \[${Reset}\]"
+export PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
