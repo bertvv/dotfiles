a6923450061d26adb3902a90418bad688ea8ac2c
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-03-20 21:09:48 +0100

    Better umask

--- .bash.d/shell.sh
+++ .bash.d/shell.sh
@@ -20,3 +20,5 @@ shopt -s globstar     # allow use of ** in file globbing
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2 | tr ' ' '\n')" scp sftp ssh
 
+# Set umask to a safer value
+umask 077
