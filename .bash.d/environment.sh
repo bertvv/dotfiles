3ff4b0c2403da4fa0e6bdcf4e4ca534c35f41a29
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2017-01-10 10:25:28 +0100

    Use most <http://www.jedsoft.org/most/> as the pager

--- .bash.d/environment.sh
+++ .bash.d/environment.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-#
-# Created:  za 14 sep 2013 02:28:09  CEST
-# Modified: vr 20 sep 2013 04:08:16  CEST
-# Author:   Bert Van Vreckem <bert.vanvreckem@gmail.com>
-#
-# 
 
 export PATH=${HOME}/bin/:${HOME}/opt/packer:${PATH}
-
+export PAGER=most
