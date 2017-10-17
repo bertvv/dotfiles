123729205fa6bdb8fcbf238588f8ec3d9efa01c6
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2017-10-17 08:35:17 +0200

    Add shortcuts for often used directories

--- .bash.d/shell.sh
+++ .bash.d/shell.sh
@@ -77,6 +77,12 @@ CDPATH=".:~:~/CfgMgmt:~/c"
 # Define a variable containing a path and you will be able to cd into it regardless of the directory you're in
 shopt -s cdable_vars
 
+export hg="${HOME}/Documents/Vakken"
+export elnx="${hg}/enterprise-linux/17-18/"
+export ilnx="${hg}/besturingssystemen/17-18/"
+export stage="${hg}/stage/17-18/"
+export p3ops="${hg}/p3ops/17-18/"
+
 # Examples:
 # export dotfiles="$HOME/dotfiles"
 # export projects="$HOME/projects"
