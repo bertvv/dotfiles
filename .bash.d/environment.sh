218b19be418ff139e073b147433e4b873ea63c3e
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2020-07-10 09:55:18 +0200

    Initialise pyenv

--- .bash.d/environment.sh
+++ .bash.d/environment.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-export PATH=${HOME}/.local/bin/:${PATH}
+export PATH=${HOME}/.pyenv/bin:${HOME}/.local/bin:${PATH}
+
+# pyenv initialization, see https://github.com/pyenv/pyenv
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
