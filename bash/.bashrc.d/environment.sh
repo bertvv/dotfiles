1c75797cf28c6d177c19c53c983c596920772d5f
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2024-12-18 01:46:53 +0100

    Add FZF options

--- bash/.bashrc.d/environment.sh
+++ bash/.bashrc.d/environment.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 export PATH=${HOME}/.local/bin:${PATH}
+
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,.local
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
