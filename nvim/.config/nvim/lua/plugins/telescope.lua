6ab3eb67ff5b3aea2b6ed7f6d74acf0c643e7f33
Bert Van Vreckem <bert.vanvreckem@hogent.be>
2025-05-06 21:25:05 +0200

    Allow telescope to search (most) hidden directories

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,63 @@
 -- telescope.lua - configure telescope fuzzy finder
 
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
+
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-  end
+
+    -- Find hidden files
+    -- Source: <https://stackoverflow.com/a/76991432/887560>
+    require('telescope').setup({
+      defaults = {
+        -- configure to use `ripgrep`
+        vimgrep_arguments = {
+          "rg",
+          "--follow",        -- Follow symbolic links
+          "--hidden",        -- Search for hidden files
+          "--no-heading",    -- Don't group matches by each file
+          "--with-filename", -- Print the file path with the matched lines
+          "--line-number",   -- Show line numbers
+          "--column",        -- Show column numbers
+          "--smart-case",    -- Smart case search
+
+          -- Exclude some patterns from search
+          "--glob=!**/.git/*",
+          "--glob=!**/.idea/*",
+          "--glob=!**/.vscode/*",
+          "--glob=!**/build/*",
+          "--glob=!**/dist/*",
+          "--glob=!**/node_modules/*",
+          "--glob=!**/yarn.lock",
+          "--glob=!**/package-lock.json",
+        },
+
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          -- needed to exclude some files & dirs from general search
+          -- when not included or specified in .gitignore
+          find_command = {
+            "rg",
+            "--files",
+            "--hidden",
+            "--glob=!**/.git/*",
+            "--glob=!**/.idea/*",
+            "--glob=!**/.vscode/*",
+            "--glob=!**/build/*",
+            "--glob=!**/dist/*",
+            "--glob=!**/yarn.lock",
+            "--glob=!**/package-lock.json",
+          },
+        },
+      },
+    })
+  end,
 }
