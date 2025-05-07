bdc6223ab82097920386d80f62c52103335ed216
Bert Van Vreckem <bert.vanvreckem@hogent.be>
2025-05-07 15:33:16 +0200

    Add descriptions to keybinds

--- nvim/.config/nvim/lua/config/cycle-colors.lua
+++ nvim/.config/nvim/lua/config/cycle-colors.lua
@@ -4,28 +4,28 @@
 
 local colorschemes = vim.fn.getcompletion("", "color")
 local colorschemes_idx = vim.fn.index(
-    colorschemes,
-    vim.api.nvim_cmd({ cmd = "colorscheme" }, { output = true })
+  colorschemes,
+  vim.api.nvim_cmd({ cmd = "colorscheme" }, { output = true })
 )
 
 local function change_colorscheme(forward)
-    colorschemes_idx = (colorschemes_idx + forward) % #colorschemes
+  colorschemes_idx = (colorschemes_idx + forward) % #colorschemes
 
-    local ok = pcall(function()
-        vim.cmd("colorscheme " .. colorschemes[colorschemes_idx])
-    end)
+  local ok = pcall(function()
+    vim.cmd("colorscheme " .. colorschemes[colorschemes_idx])
+  end)
 
-    if not ok then
-        change_colorscheme(forward)
-    end
+  if not ok then
+    change_colorscheme(forward)
+  end
 
-    print(colorschemes[colorschemes_idx])
+  print(colorschemes[colorschemes_idx])
 end
 
 vim.keymap.set("n", "<C-h>", function()
-    change_colorscheme(1)
-end)
+  change_colorscheme(1)
+end, { desc = "Select next color scheme" })
 
 vim.keymap.set("n", "<C-l>", function()
-    change_colorscheme(-1)
-end)
+  change_colorscheme(-1)
+end, { desc = "Select previous color scheme" })
--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -7,35 +7,35 @@ vim.g.mapleader = " "
 -- <leader><leader> - source the current file
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-    print("current file sourced")
-end)
+  vim.cmd("so")
+  print("current file sourced")
+end, { desc = "Source current file" })
 
 -- move through buffers
 
-vim.keymap.set("n", "<leader>n", ":bn<cr>")
-vim.keymap.set("n", "<leader>p", ":bp<cr>")
-vim.keymap.set("n", "<leader>x", ":bx<cr>")
+vim.keymap.set("n", "<leader>n", ":bn<cr>", { desc = "Open next buffer" })
+vim.keymap.set("n", "<leader>p", ":bp<cr>", { desc = "Open previous buffer" })
+vim.keymap.set("n", "<leader>x", ":bx<cr>", { desc = "Close current buffer" })
 
 -- yank to clipboard
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard" })
 
 -- format file
 
-vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { desc = "Format buffer" })
 
 -- cycle through diagnostics
 
 vim.keymap.set("n", "<leader>dn", function()
-    vim.diagnostic.jump({ count = 1, float = true })
-end)
+  vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Jump to next diagnostic issue" })
 vim.keymap.set("n", "<leader>dp", function()
-    vim.diagnostic.jump({ count = -1, float = true })
-end)
+  vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Jump to previous diagnostic issue" })
 vim.keymap.set("n", "<leader>dl", function()
-    vim.diagnostic.setloclist()
-end)
+  vim.diagnostic.setloclist()
+end, { desc = "List diagnostic issues" })
 vim.keymap.set("n", "<leader>df", function()
-    vim.diagnostic.open_float()
+  vim.diagnostic.open_float()
 end)
